a525147ed458c0f4c6ce996ef4cda80601fb3978
amanda-barrett <amanda.barrett1@marist.edu>
2026-04-08 22:51:46 -0400

    Create Goblin.java

---
+++ assignment7/Goblin.java
@@ -0,0 +1,12 @@
+public class Goblin extends Enemy{
+
+    //Needs constructor
+    public Goblin(int h, int d) {
+        super(h, d);
+    }
+    //needs method to overwrite attack in enemy to say "the goblin gobbles" instead of "the enemy attacks
+    @Override
+    void attack(){
+        System.out.println("the goblin gobbles");
+    }
+}
